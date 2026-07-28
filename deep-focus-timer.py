19c82140839972d75028083ac771219149aec9cd
yvonne chen <121yvonnechen@gmail.com>
2026-07-28 14:47:52 +1200

    add basic focus count down

--- deep-focus-timer.py
+++ deep-focus-timer.py
@@ -2,17 +2,19 @@
 author: yvonne
 function: build a timer for studying, manage your time, take a long term focus, then have a little break
 '''
-
-time_mode = [1, 2, 3, 4]
+import time
+time_mode = [1, 2, 3, 4, 5]
 time_mode_detail = {
     1:(25, 5),
     2:(50, 10), 
     3:(100, 15), 
-    4:(150, 25)
+    4:(150, 25),
+    5:(0.5, 0)
     }
 
 
 def ask_user_mode():
+    # ask user to choose a timer mode
     print("Time mode: \n" \
             "1: Focus 25 minutes, break 5 minutes \n" \
             "2: Focus 50 minutes, break 10 minures \n" \
@@ -33,13 +35,35 @@ def ask_user_mode():
     return user_choose_mode
 
 def get_time_mode(user_choose_mode):
+    # get time mode detail
     focus_time, break_time = time_mode_detail[user_choose_mode]
     return focus_time, break_time
 
 def convert_time_mode_second(focus_time, break_time):
+    # convert minutes to seconds for next step count down
     focus_time = focus_time * 60
     break_time = break_time * 60
     return focus_time, break_time
+
+
+def timer_start_prompt():
+    # ask user to start the timer
+    user_start = None
+    while user_start not in ("Y", "N"):
+        user_start = input(
+            "Start the timer? \n"
+            "(Y for yes, N for no): "
+            ).strip().upper()
+        if user_start not in ("Y", "N"):
+            print("Please enter only Y or N")
+
+    return user_start == "Y"
+
+
+def count_dowe(seconds):
+    # every second count down.
+    time.sleep(seconds)
+    print("Focus session completed!")
 
 
 
@@ -49,5 +73,14 @@ def main():
     focus_second, break_second = convert_time_mode_second(focus_time, break_time)
     print("Focus time: {} minutes".format(focus_time))
     print("Break time : {} minutes".format(break_time))
-    print(focus_second, break_second)
+    if timer_start_prompt():
+        print(
+            "Timer now starts! \n"
+            "Do not check the timer. \n"
+            )
+        count_dowe(focus_second)
+    else:
+        print("Timer cancelled.")
+
+
 main()
